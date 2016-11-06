e18c2517fd077f395463bacae54c208283baec14
danchiem <danchiem@my.ccsu.edu>
2016-11-06 17:21:20 -0500

    Fixed version of busStopCoord
    stored coordinates in 2-d array and better list of bus stops

--- busStopCoord/src/busstopcoord/BusStopCoord.java
+++ busStopCoord/src/busstopcoord/BusStopCoord.java
@@ -1,10 +1,6 @@
-/*
-Team unqiue name
- */ 
 package busstopcoord;
-
 /*
- * @author DanC
+ * @author DanC Team unique name
  */
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -13,12 +9,12 @@ import java.io.IOException;
 
 public class BusStopCoord {
   
-//  Using BufferedReader and FileReader
-// 
     public static void main(String[] args) throws FileNotFoundException, IOException
     {
-        String fileName = "stops.txt";
+        String fileName = "haNbWastops.txt";
         String line = null;
+        float[][] Coord = new float[5959][2];
+        int index = 0;
         
         try{
             FileReader fileReader = new FileReader(fileName);
@@ -26,19 +22,27 @@ public class BusStopCoord {
             try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
                 while ((line = bufferedReader.readLine()) != null)
                 {  
-                    // using data from https://www.cttransit.com/about/developers zip files. NOTE delete the the first line from stops.txt for it to work 
+                    // using data from https://www.cttransit.com/about/developers zip files. NOTE delete the the first line from stops.txt and combine 
+                    // Hartford, Newbritain, and Waterbury stops.txts into one file for it to work 
                     // [0] = stop_id,[1] = stop_code,[2] = stop_name,[3] = stop_lat,[4] = stop_lon,
                     // [5] = zone_id, [6] = stop_url,[7] = location_type,[8] parent_station
-                            String[] tokenize = line.split(",");  
-                            
-                                          
+                            String[] tokenize = line.split(",");                 
                             float lat = Float.parseFloat(tokenize[3]);
-                            float lot = Float.parseFloat(tokenize[4]);
-                            System.out.println(lat + " " + lot );                
-                }
+                            float lon = Float.parseFloat(tokenize[4]);
+                       
+                      //    System.out.println(lat + " " + lon );       // test for getting lat and lon      
+                            Coord[index][0] =  lat;
+                            Coord[index][1] = lon;
+                            index++;
+                            
+                }         
+//                for( index = 0; index < Coord.length; index++) // printing the array out to test if lat and lon is corretly stored
+//                {
+//                    System.out.println(Coord[index][0] + "" + Coord[index][1]);
+//                }
+                  
             }
-        }
-        
+        }    
         catch(FileNotFoundException ex){
             System.out.println( "Cannot open " + fileName + "");
         }
