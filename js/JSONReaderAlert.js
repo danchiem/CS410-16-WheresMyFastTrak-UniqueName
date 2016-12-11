69479904ef2e33c55258fe383de67ea12d0983b2
Ilinskip <ilinskip@my.ccsu.edu>
2016-12-10 23:52:28 -0500

    Add some alert json functions
    So far I only added the basic display of the Alert json if these
    conditions are met : the end time has not passed.

--- js/JSONReaderAlert.js
+++ js/JSONReaderAlert.js
@@ -1,11 +1,44 @@
 window.onload = alerts;     
-var jsonObjectAlert;
 
+var alertToPost;
 function alerts()
 {
-		jQuery.getJSON("http://65.213.12.244/realtimefeed/alert/alerts.json", function(data){
-			jsonObjectAlert = jQuery.parseJSON(JSON.stringify(data));
-			console.log(jsonObjectAlert);
-		});
-		setTimeout("alerts()",5000);
+	var jsonObjectAlert;
+	jQuery.getJSON("http://65.213.12.244/realtimefeed/alert/alerts.json",
+	 function(data){
+		jsonObjectAlert = jQuery.parseJSON(JSON.stringify(data));
+		console.log(jsonObjectAlert);
+
+		var seconds = new Date().getTime() / 1000;
+
+		if(seconds < jsonObjectAlert.entity.alert.active_period.end  && jsonObjectAlert.entity.alert.header_text.text !== alertToPost.entity.alert.header_text.text) {
+
+			//for now do nothing
+
+		} else {
+			alertToPost = jsonObjectAlert;
+		}
+
+	});
+
+	var alertInfo = 	'<div id="content">'+
+			            '<div id="siteNotice">'+
+			           	'</div>'+
+			          	'<h1 id="firstHeading" class="firstHeading">'+ alertToPost.entity.alert.header_text.text +'</h1>'+
+			           	'<div id="bodyContent">' +
+				            '<p>' +
+					    	   	'Description: ' + alertToPost.entity.alert.description_text.text + '<br>' +
+					       	   	'Stop ID: ' + alertToPost.entity.alert.informed_entity.stop_id + '<br>' +
+					      	   	'Route ID: ' + alertToPost.entity.alert.informed_entity.route_id + '<br>' +
+					      	   	'Start/End Time: ' + toTimeString( alertToPost.entity.alert.active_period.start ) + ' / ' + toTimeString( alertToPost.entity.alert.active_period.end ) +'<br>' 
+				            '</p>'+
+			            '</div>'+
+		            	'</div>';
+	return bsinfo;
+
+	setTimeout("alerts()",30000);
 };
+
+function toTimeString(seconds) {
+  return (new Date(seconds * 1000)).toUTCString().match(/(\d\d:\d\d:\d\d)/)[0];
+}
